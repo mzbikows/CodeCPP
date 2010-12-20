520aa3f3dff5ab29042e4ec37a2c7c4e30aba598
mzbikows <mzbikows@gmail.com>
2010-12-20 22:19:36 +0100

    marios

--- Hello.cpp
+++ Hello.cpp
@@ -1,18 +1,1 @@
-sdfsdfdsfdsf
-
-
-d
-sfdsfsdfsdsd
-# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-# On branch master
-#
-# Initial commit
-#
-# Changes to be committed:
-#   (use "git rm --cached <file>..." to unstage)
-#
-#	new file:   Hello.cpp
-#cxxc
-sdfsd
-sd
+ad
