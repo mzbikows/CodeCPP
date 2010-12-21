61008f17850e6a5e370925bd00c91d2d78527cda
mzbikows <mzbikows@gmail.com>
2010-12-21 11:10:20 +0100

    Wersje Zadanie7

---
+++ wskaznik.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+ int main ()
+ {
+   int liczba = 80;
+   int *wskaznik = &liczba;
+   
+   cout << liczba << " jjjj" << &liczba << endl;
+   cout << wskaznik << " " << *wskaznik << endl;
+ 
+   *wskaznik = 42;
+   cout << liczba << " " << *wskaznik << endl;
+   
+   liczba =32;
+   cout << liczba << " " << *wskaznik << endl;
+   
+   liczba =0x30;
+   cout << liczba << " " << *wskaznik << endl;
+ 
+   return 0;
+ }
